0d24238f50e4c3cf224126c24b671b2766c35d21
Alberto <Alberto@HAF-932>
2014-11-10 22:35:32 +0100

    Añadido boton guardar y preguntar al salir.

--- Loteria/src/com/iesfernandowirtz/loteria/MainActivity.java
+++ Loteria/src/com/iesfernandowirtz/loteria/MainActivity.java
@@ -1,12 +1,16 @@
 package com.iesfernandowirtz.loteria;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +18,8 @@ public class MainActivity extends Activity {
 	TextView textViewResultado;
 	TextView textViewReintegro;
 	Loteria objLoteria;
+	String resultado;
+	String reintegro;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +36,88 @@ public class MainActivity extends Activity {
 
 	public void onButtonClickGenerar(View v) {
 
-		objLoteria = new Loteria();
-		String resultado = objLoteria.mostrarTabla();
-		String reintegro = objLoteria.mostrarReintegro();
-		
+		generarNumeros();
+
+		mostrarNumeros();
+
+	}
+
+	private void mostrarNumeros() {
 		textViewResultado.setText(resultado);
 		textViewReintegro.setText(reintegro);
-		
-		SharedPreferences preferencias=getSharedPreferences("datos",Context.MODE_PRIVATE);
-		Editor editor=preferencias.edit();
+	}
+
+	private void generarNumeros() {
+		objLoteria = new Loteria();
+		resultado = objLoteria.mostrarTabla();
+		reintegro = objLoteria.mostrarReintegro();
+	}
+
+	private void guardarDatos(String resultado, String reintegro) {
+		SharedPreferences preferencias = getSharedPreferences("datos",
+				Context.MODE_PRIVATE);
+		Editor editor = preferencias.edit();
 		editor.putString("resultado", resultado);
 		editor.putString("reintegro", reintegro);
-		editor.commit();  
+		editor.commit();
+	}
 
+	public void onButtonClickGuardar(View v) {
+		if (resultado == null || reintegro == null) {
+			alertaSimple("Error al guardar",
+					"Debe de generar un número para poder guardarlo");
+
+		} else {
+			guardarDatos(resultado, reintegro);
+			alertaSimple("Guardado correcto",
+					"El número se ha guardado correctamente");
+		}
+	}
+
+	private void alertaSimple(String titulo, String mensaje) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(titulo);
+		builder.setMessage(mensaje);
+		builder.setPositiveButton("OK", null);
+		builder.create();
+		builder.show();
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+			new AlertDialog.Builder(this)
+					.setIcon(android.R.drawable.ic_dialog_alert)
+					.setTitle("Salir")
+					.setMessage(
+							"Estás seguro? Se perderan los cambios no guardados!")
+					.setNegativeButton(android.R.string.cancel, null)
+					// sin listener
+					.setPositiveButton(android.R.string.ok,
+							new DialogInterface.OnClickListener() {// un
+								// listener
+								// que al
+								// pulsar,
+								// cierre la
+								// aplicacion
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// Salir
+									MainActivity.this.finish();
+								}
+							}).show();
+
+			// Si el listener devuelve true, significa que el evento esta
+			// procesado, y nadie debe hacer nada mas
+			return true;
+		}
+		// para las demas cosas, se reenvia el evento al listener habitual
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
